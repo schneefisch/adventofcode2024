4fad9bd42673892c6ceae988cca58e8069e1413b
ROESEFL <florian.roeser@exxeta.com>
2024-12-02 12:03:55 +0100

    reordered directory structure

--- challenges/util/csv.go
+++ challenges/util/csv.go
@@ -1,77 +1,77 @@
 package util
 
 import (
-    "bufio"
-    "encoding/csv"
-    "log"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // ReadCSV reads a CSV file and returns a slice of slices of strings.
 func ReadCSV(filename string) ([][]string, error) {
-    // read file
-    file, err := os.Open(filename)
-    if err != nil {
-        return nil, err
-    }
+	// read file
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
 
-    // close file
-    defer func(file *os.File) {
-        err := file.Close()
-        if err != nil {
-            log.Fatal(err)
-        }
-    }(file)
+	// close file
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
-    csvReader := csv.NewReader(file)
-    data, err := csvReader.ReadAll()
-    if err != nil {
-        return nil, err
-    }
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
 
-    return data, nil
+	return data, nil
 }
 
 // ReadSpaceSeparatedData reads a space-separated file and returns a slice of slices of strings.
 func ReadSpaceSeparatedData(filename string) ([][]int, error) {
-    // read file
-    file, err := os.Open(filename)
-    if err != nil {
-        return nil, err
-    }
+	// read file
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
 
-    // close file
-    defer func(file *os.File) {
-        err := file.Close()
-        if err != nil {
-            log.Fatal(err)
-        }
-    }(file)
+	// close file
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
-    // read data line-by-line
-    data := make([][]int, 0)
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lineStr := scanner.Text()
-        line := strings.Split(lineStr, " ")
-        //log.Println(line)
+	// read data line-by-line
+	data := make([][]int, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineStr := scanner.Text()
+		line := strings.Split(lineStr, " ")
+		//log.Println(line)
 
-        numbers := make([]int, 0)
-        for _, numStr := range line {
-            num, err := strconv.Atoi(numStr)
-            if err != nil {
-                return nil, err
-            }
-            numbers = append(numbers, num)
-        }
+		numbers := make([]int, 0)
+		for _, numStr := range line {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				return nil, err
+			}
+			numbers = append(numbers, num)
+		}
 
-        data = append(data, numbers)
-    }
-    if err = scanner.Err(); err != nil {
-        return nil, err
-    }
+		data = append(data, numbers)
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
 
-    return data, nil
+	return data, nil
 }
