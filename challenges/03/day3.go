ab7e832dee3c6598181b3977aaf3cbae27b4efa1
ROESEFL <florian.roeser@exxeta.com>
2024-12-03 09:11:07 +0100

    refactor: some simplifications and updated comments

--- challenges/03/day3.go
+++ challenges/03/day3.go
@@ -11,24 +11,20 @@ import (
 
 // MulNumbers is a struct that holds a list of the two numbers
 type MulNumbers struct {
-	n1 int
-	n2 int
+	n1, n2 int
 }
 
+// DaythreeMullitover reads a file, processes its content to find and multiply numbers, and returns the result.
+// It takes a filename as input and returns the result of the added multiplications.
 func DaythreeMullitover(filename string) (int, error) {
 	lines, err := util.ReadLines(filename)
 	if err != nil {
 		return 0, err
 	}
 	// add all lines into one string
-	line := ""
-	for _, l := range lines {
-		line += l
-	}
-
+	line := strings.Join(lines, "")
 	// remove don't instructions
 	line = removeDonts(line)
-
 	log.Println(line)
 
 	// get all mull-instances
@@ -48,36 +44,29 @@ func DaythreeMullitover(filename string) (int, error) {
 	return result, nil
 }
 
-// parse through the string and remove all characters that are in between a
-// "don't()" a "do()" instruction
+// removeDonts parses through the string and removes all characters that are in between a
+// "don't()" and a "do()" instruction.
+// It takes a string as input and returns the modified string.
 func removeDonts(line string) string {
 	// split string at "don't()" and "do()"
 	splitDonts := strings.Split(line, "don't()")
-	log.Println(splitDonts)
-
 	// it starts with valid-expression, so the first part is always valid
 	validInstructions := splitDonts[0]
 	// iterate through the rest of the split
 	for _, part := range splitDonts[1:] {
-		// split at a "do()" instruction
-		splitDos := strings.Split(part, "do()")
-		// add only the second part, after the "do()" instruction if it exists
-		if len(splitDos) > 1 {
-			// add all consecutive do's
-			for _, do := range splitDos[1:] {
-				validInstructions += do
-			}
+		// split at all "do()" instructions
+		// add only the second and consecutive parts, after the "do()" instruction if it exists
+		if splitDos := strings.Split(part, "do()"); len(splitDos) > 1 {
+			validInstructions += strings.Join(splitDos[1:], "")
 		}
 	}
-
 	return validInstructions
 }
 
-/*
- * parseMulNumbers parses the mul-instance String and returns a list of MulNumbers.
- */
+// parseMulNumbers parses the mul-instance strings and returns a list of MulNumbers.
+// It takes a slice of strings as input and returns a slice of MulNumbers and an error if any.
 func parseMulNumbers(mulInstances []string) ([]MulNumbers, error) {
-	mulMap := make([]MulNumbers, 0)
+	var mulMap []MulNumbers
 	for _, mulInst := range mulInstances {
 		// get the two numbers from the mul-instance
 		n1, n2, err := parseMulNumbersFromInstance(mulInst)
@@ -89,9 +78,8 @@ func parseMulNumbers(mulInstances []string) ([]MulNumbers, error) {
 	return mulMap, nil
 }
 
-/*
- * parseMulNumbersFromInstance parses the two numbers from a single mul-instance String.
- */
+// parseMulNumbersFromInstance parses the two numbers from a single mul-instance String.
+// It takes a string as input and returns two integers and an error if any.
 func parseMulNumbersFromInstance(mulInst string) (int, int, error) {
 	// regex to match the numbers
 	re := regexp.MustCompile("\\d{1,3}")
@@ -113,9 +101,8 @@ func parseMulNumbersFromInstance(mulInst string) (int, int, error) {
 	return n1, n2, nil
 }
 
-/*
- * findAllMulInstances finds all mul-instances in a given string.
- */
+// findAllMulInstances finds all mul-instances in a given string.
+// It takes a string as input and returns a slice of strings containing all mul-instances.
 func findAllMulInstances(line string) []string {
 	// regular expression to match the mull-instances
 	re := regexp.MustCompile("mul\\(\\d{1,3},\\d{1,3}\\)")
