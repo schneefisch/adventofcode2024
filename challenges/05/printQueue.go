d5d194d5b8765b98354dcfd6c3339f69f1af86b3
ROESEFL <florian.roeser@exxeta.com>
2024-12-05 12:27:26 +0100

    added solution for day5

---
+++ challenges/05/printQueue.go
@@ -0,0 +1,241 @@
+package _5
+
+import (
+	"adventofcode2024/challenges/util"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Update struct {
+	pages []int
+}
+
+type Graph struct {
+	vertices map[int][]int
+}
+
+func newGraph() *Graph {
+	return &Graph{
+		vertices: make(map[int][]int),
+	}
+}
+
+func (g *Graph) addEdge(u, v int) {
+	g.vertices[u] = append(g.vertices[u], v)
+}
+
+func PrintQueue(filenameOrdering, filenameUpdates string) (int, int, error) {
+	// read files
+	ordering, err := util.ReadLines(filenameOrdering)
+	if err != nil {
+		return 0, 0, err
+	}
+	updateStrings, err := util.ReadLines(filenameUpdates)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// parse ordering
+	graph, err := parseOrdering(ordering)
+	if err != nil {
+		return 0, 0, err
+	}
+	log.Printf("Parsed Ordering-Rules: %v", graph)
+
+	// parse updateStrings
+	updates, err := parseUpdates(updateStrings)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// filter out the updates that are incorrect
+	validUpdate, invalidUpdates := filterUpdates(updates, graph)
+
+	log.Printf("Updates: %v", validUpdate)
+
+	// add the middle-numbers of the updates
+	sum := 0
+	for _, update := range validUpdate {
+		wantPageAtIndex := len(update.pages) / 2
+		sum += update.pages[wantPageAtIndex]
+	}
+
+	// fix ordering of invalid updates
+	invalidUpdates = fixOrdering(invalidUpdates, graph)
+	log.Printf("Invalid Updates ordered: %v", invalidUpdates)
+	sumInvalid := 0
+	for _, update := range invalidUpdates {
+		wantPageAtIndex := len(update.pages) / 2
+		sumInvalid += update.pages[wantPageAtIndex]
+	}
+
+	return sum, sumInvalid, nil
+}
+
+// fixOrdering iterates through all updates and fixes the ordering for each
+func fixOrdering(invalidUpdates []Update, graph *Graph) []Update {
+	// topologically sort the rules
+	for i, update := range invalidUpdates {
+		update = fixPageOrdering(update, graph)
+
+		invalidUpdates[i] = update
+	}
+
+	return invalidUpdates
+}
+
+// fixPageOrdering takes an update and fixes the ordering of the pages
+func fixPageOrdering(update Update, graph *Graph) Update {
+	result := Update{pages: make([]int, len(update.pages))}
+	// iterate through the pages and copy them into place
+	for _, page := range update.pages {
+		// for all consecutive pages we need to go through the already copied pages and check if the current page has
+		// to be inserted before the inserted page
+		for j, insertedPage := range result.pages {
+			// if the inserted page number is 0, we can insert the page at this place
+			if insertedPage == 0 {
+				result.pages[j] = page
+				break
+			}
+			// check if the page has to be inserted before the existing page
+			if laterNeighbours, ok := graph.vertices[page]; ok {
+				if contains(laterNeighbours, insertedPage) {
+					log.Printf("Page [%d] has to be inserted before page [%d]", page, insertedPage)
+					// shift all pages from that index to the right and insert the page at the current index
+					for k := len(result.pages) - 1; k > j; k-- {
+						result.pages[k] = result.pages[k-1]
+					}
+					result.pages[j] = page
+					break
+				}
+			}
+			// the page is not mentioned in the ordering rules, so we can insert it at the end
+		}
+	}
+
+	return result
+}
+
+// filterUpdates splits the updates into valid and invalid updates
+func filterUpdates(updates []Update, orderingRules *Graph) ([]Update, []Update) {
+	// iterate through all updates
+	corruptedUpdates := make([]int, 0)
+	for i, update := range updates {
+		isCorrupted, corruptedPageIndex := isCorruptedUpdate(update, orderingRules, i)
+		if isCorrupted {
+			log.Printf("Update [%d] is corrupted at page [%d]", i, update.pages[corruptedPageIndex])
+			// remove the update
+			corruptedUpdates = append(corruptedUpdates, i)
+		}
+	}
+
+	invalidUpdates := make([]Update, 0)
+	validUpdates := make([]Update, 0)
+	for i, update := range updates {
+		// only add the update if it is not in the corruptedUpdates
+		if contains(corruptedUpdates, i) {
+			invalidUpdates = append(invalidUpdates, update)
+		} else {
+			validUpdates = append(validUpdates, update)
+		}
+	}
+	return validUpdates, invalidUpdates
+}
+
+// isCorruptedUpdate checks if an update is valid according to the orderingRules
+// returns true if the update is corrupted
+func isCorruptedUpdate(update Update, orderingRules *Graph, i int) (bool, int) {
+	isCorrupted := false
+	corruptedPageIndex := -1
+	// iterate through all pages
+pageLoop:
+	for pageIndex, page := range update.pages {
+		// check if the page is in the orderingRules
+		if rule, ok := orderingRules.vertices[page]; ok {
+			// page is in the orderingRules
+			//log.Printf("Update [%d] - Page [%d] is in the orderingRules - rule: %v", i, page.number, rule)
+			// check the index of the page and compare it with the indices of all of the other pages in the rule.
+			// the page must have a lower index than all of the other pages in the rule
+			for _, otherPage := range rule {
+				// find the index of the other page in the "update"
+				otherPageIndex := -1
+				for j, pageInUpdate := range update.pages {
+					if pageInUpdate == otherPage {
+						otherPageIndex = j
+						break
+					}
+				}
+				// check if the otherPageIndex is before the current pageIndex
+				if otherPageIndex >= 0 && otherPageIndex < pageIndex {
+					// the other page is before the current page
+					//log.Printf("Update [%d] - Page [%d] is before Page [%d], corrupted Update", i, page,
+					//    otherPage)
+					isCorrupted = true
+					corruptedPageIndex = pageIndex
+					break pageLoop
+				}
+			}
+		}
+	}
+	return isCorrupted, corruptedPageIndex
+}
+
+// contains checks if an integer is in an array
+func contains(array []int, i int) bool {
+	for _, value := range array {
+		if value == i {
+			return true
+		}
+	}
+	return false
+}
+
+// parseUpdates parses the input strings into a 2D slice of integers
+func parseUpdates(input []string) ([]Update, error) {
+	result := make([]Update, 0)
+	for _, line := range input {
+		// split lines
+		numberStrings := strings.Split(line, ",")
+		update := Update{pages: make([]int, 0)}
+		for _, numberString := range numberStrings {
+			// parse to integers
+			number, err := strconv.Atoi(numberString)
+			if err != nil {
+				return result, fmt.Errorf("invalid number: %s - %v", line, err)
+			}
+			update.pages = append(update.pages, number)
+		}
+		result = append(result, update)
+	}
+
+	return result, nil
+}
+
+// parseOrdering parses the input strings into a map of integers where the key is the first number and the
+// value is a slice of all integers that must be printed later
+func parseOrdering(ordering []string) (*Graph, error) {
+	graph := newGraph()
+	for _, line := range ordering {
+		// split lines
+		var a, b int
+		numberStrings := strings.Split(line, "|")
+		if len(numberStrings) != 2 {
+			return graph, fmt.Errorf("invalid line: %s", line)
+		}
+		// parse to integers
+		a, err := strconv.Atoi(numberStrings[0])
+		if err != nil {
+			return graph, fmt.Errorf("invalid first number: %s - %v", line, err)
+		}
+		b, err = strconv.Atoi(numberStrings[1])
+		if err != nil {
+			return graph, fmt.Errorf("invalid second number: %s - %v", line, err)
+		}
+		// check if a is already in the map
+		graph.addEdge(a, b)
+	}
+
+	return graph, nil
+}
---
+++ challenges/05/printQueue_test.go
@@ -0,0 +1,114 @@
+package _5
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPrintQueue(t *testing.T) {
+	type args struct {
+		filenameOrdering string
+		filenameUpdates  string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want        int
+		wantInvalid int
+		wantErr     bool
+	}{
+		{
+			name: "Test case 1",
+			args: args{
+				filenameOrdering: "test_rules.txt",
+				filenameUpdates:  "test_updates.txt",
+			},
+			want:        143,
+			wantInvalid: 123,
+			wantErr:     false,
+		},
+		{
+			name: "input",
+			args: args{
+				filenameOrdering: "input_rules.txt",
+				filenameUpdates:  "input_updates.txt",
+			},
+			want:        4774,
+			wantInvalid: 6004,
+			wantErr:     false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotValid, gotInvalid, err := PrintQueue(test.args.filenameOrdering, test.args.filenameUpdates)
+			if (err != nil) != test.wantErr {
+				t.Errorf("PrintQueue() error = %v, wantErr %v", err, test.wantErr)
+				return
+			}
+			if gotValid != test.want {
+				t.Errorf("PrintQueue() gotValid = %v, want %v", gotValid, test.want)
+			} else {
+				t.Logf("PrintQueue() gotValid = %v, want %v", gotValid, test.want)
+			}
+			if gotInvalid != test.wantInvalid {
+				t.Errorf("PrintQueue() gotInvalid = %v, want %v", gotInvalid, test.wantInvalid)
+			} else {
+				t.Logf("PrintQueue() gotInvalid = %v, want %v", gotInvalid, test.wantInvalid)
+			}
+		})
+	}
+}
+
+func Test_fixPageOrdering(t *testing.T) {
+	graph := &Graph{
+		vertices: map[int][]int{
+			29: {13},
+			47: {53, 13, 61, 29},
+			53: {29, 13},
+			61: {13, 53, 29},
+			75: {29, 53, 47, 61, 13},
+			97: {13, 61, 47, 29, 53, 75},
+		},
+	}
+	type args struct {
+		update Update
+		graph  *Graph
+	}
+	tests := []struct {
+		name string
+		args args
+		want Update
+	}{
+		{
+			name: "Test case 1",
+			args: args{
+				update: Update{pages: []int{75, 97, 47, 61, 53}},
+				graph:  graph,
+			},
+			want: Update{pages: []int{97, 75, 47, 61, 53}},
+		},
+		{
+			name: "Test case 2",
+			args: args{
+				update: Update{pages: []int{61, 13, 29}},
+				graph:  graph,
+			},
+			want: Update{pages: []int{61, 29, 13}},
+		},
+		{
+			name: "Test case 3",
+			args: args{
+				update: Update{pages: []int{97, 13, 75, 29, 47}},
+				graph:  graph,
+			},
+			want: Update{pages: []int{97, 75, 47, 29, 13}},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got := fixPageOrdering(test.args.update, test.args.graph); !reflect.DeepEqual(got, test.want) {
+				t.Errorf("fixPageOrdering() = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
